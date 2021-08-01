72fb8d62016d66b73380c120b051be1440896e1c
potatoking145 <collinmurry2@gmail.com>
2021-08-01 14:46:10 -0500

    replaced EnTT with flecs

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,18 @@
 #include <iostream>
-#include <entt/entt.hpp>
 #include <libtcod.hpp>
+#include "flecs.h"
+
+struct ex 
+{
+	int x;
+};
 
 int main()
 {
-	std::cout << "Hello World!";
+	std::cout << "Hello World!" << std::endl;
 
-	TCODConsole::initRoot(60, 60, "Empty", false, TCOD_RENDERER_SDL);
-	TCODSystem::setFps(60);
-
-	while(true) {}
+	//TCODConsole::initRoot(60, 60, "Empty", false, TCOD_RENDERER_SDL);
+	//TCODSystem::setFps(60);
 
 	return 0;
 }
