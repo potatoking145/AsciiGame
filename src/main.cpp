09207d55b6ea5e9ad49b8caa5a89c9184e75dfd4
potatoking145 <collinmurry2@gmail.com>
2021-09-02 21:15:31 -0500

    added SDL2 proper

--- src/main.cpp
+++ src/main.cpp
@@ -1,144 +1,46 @@
 #include <iostream>
-#include <flecs.h>
-#include <string>
-#include <cereal/types/memory.hpp>
-#include <cereal/archives/binary.hpp>
-#include <fstream>
+#include <unordered_map>
+#include <libtcod/libtcod.h>
+#include <sdl/SDL.h>
+//std::unordered_multimap<
 
-struct position
+int main(int argc, char* argv[])
 {
-	int x;
-	int y;
+	tcod::ConsolePtr console = tcod::new_console(80, 25);
 
-	template<class Archive>
-	void serialize(Archive& ar) {
-		ar(x, y);
-	}
-};
+	TCOD_ContextParams params{};
+	params.tcod_version = TCOD_COMPILEDVERSION;  // This is required.
+	params.columns = console->w;  // Derive the window size from the console size.
+	params.rows = console->h;
+	// params.console = console.get();  // New in version 1.19, this replaces the above two lines.
+	params.window_title = "Libtcod Project";
+	params.vsync = true;
+	params.argc = argc;  // This allows some user-control of the context.
+	params.argv = argv;
 
-struct name
-{
-	std::string n;
+	tcod::ContextPtr context = tcod::new_context(params);
 
-	template<class Archive>
-	void serialize(Archive& ar) {
-		ar(n);
-	}
-};
-
-struct extra
-{
-	flecs::entity_t dummy;
-
-	template<class Archive>
-	void serialize(Archive& ar) {
-		ar(dummy);
-	}
-};
-
-std::map<flecs::entity_t, flecs::entity> entities;
-flecs::world world;
-
-void save()
-{
-	std::ofstream os("out.cereal", std::ios::binary);
-	cereal::BinaryOutputArchive outa(os);
-
-	for (auto const& [id, e] : entities) {
-		outa(id);
-
-		if (e.is_alive() == false) {
-			outa(false);
-
-			continue; //skips trying to add components from a dead entity
-		}
-		else {
-			outa(true);
-		}
-
-		auto c_position = e.get<position>();
-		if (c_position != nullptr) {
-			outa(1);
-			position comp{ c_position->x, c_position->y };
-			outa(comp);
-		}
-
-		auto c_name = e.get<name>();
-		if (c_name != nullptr) {
-			outa(2);
-			name comp{ c_name->n };
-			outa(comp);
-		}
-
-		outa(0); //marks the end of the entity components
-	}
-
-	outa(4294967295); //marks the end of the save file
-}
-
-void load()
-{
-	std::ifstream is("out.cereal", std::ios::binary);
-	cereal::BinaryInputArchive ina(is);
-
-	flecs::entity_t t;
-	int comp_id = 1; //not 0
-	bool is_alive;
-	flecs::entity entity;
+	tcod::print(*console, 0, 0, "Hello World", nullptr, nullptr, TCOD_BKGND_SET, TCOD_LEFT);
 
 	while (true) {
-		ina(t);
-		if (t == 4294967295) { break; } //reached the end
+		context->present(*console);  // Updates the visible display.
 
-		entity = world.entity(t); //gets entity from entity id
+		SDL_Event event;
+		SDL_WaitEvent(nullptr);  // Optional, sleep until events are available.
+		while (SDL_PollEvent(&event)) {
+			std::cout << event.key.keysym.sym << std::endl;
 
-		ina(is_alive);
-		if (is_alive == false) {
-			entity.destruct();
+			switch (event.type) {
+			case SDL_QUIT:
+				return 0;  // Exit.
+			}
 
-			continue;
-		};
-
-		entities[entity.id()] = entity; //adds the entity if it does not already exist ;; note: is after checking for aliveness
-
-		while (comp_id != 0) {
-			ina(comp_id);
-
-			switch (comp_id) {
-			case 1:
-				position c_pos;
-				ina(c_pos);
-				entity.set<position>({ c_pos.x, c_pos.y });
-				break;
-			case 2:
-				name c_name;
-				ina(c_name);
-				entity.set<name>({ c_name.n });
-				break;
+			if (event.key.keysym.sym == SDLK_F1) {
+				tcod::print(*console, 0, 1, "HERE!!!!!", nullptr, nullptr, TCOD_BKGND_SET, TCOD_LEFT);
+				std::cout << "h" << std::endl;
 			}
 		}
 	}
-}
-
-int main()
-{
-	entities[100001] = world.entity(100001)
-		.set<position>({ 1,2 })
-		.set<name>({ "entity_1" });
-
-	entities[100002] = (world.entity(100002)
-		.set<position>({ 5,10 })
-		.set<name>({ "entity_2" }));
-
-	entities[100002].destruct();
-
-	save();
-
-	load();
-
-	std::cout << entities[100002].is_alive();
-
-	system("pause");
 
 	return 0;
 }
