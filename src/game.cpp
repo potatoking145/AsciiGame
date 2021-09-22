527aff00dc7e4d69adc4cccd6f79a2ad147eeb2c
potatoking145 <collinmurry2@gmail.com>
2021-09-22 11:34:40 -0500

    cleanup

--- src/game.cpp
+++ src/game.cpp
@@ -9,8 +9,4 @@ void game::Game::Init()
 	_ctx.console = tcod::new_console(80, 25);
 	_ecs_world.set_target_fps(60);
 	_interface.InitContext("Greenfingers", true, SDL_WINDOW_RESIZABLE, TCOD_RENDERER_SDL2, &_ctx.console);
-
-	_ecs_world.entity() //test entity
-		.set<comp_position>({ 1,1 })
-		.set<comp_char>({ 'A' });
 }
--- src/main.cpp
+++ src/main.cpp
@@ -2,22 +2,25 @@
 #include "game.h"
 #include "window.h"
 
-int main(int argc, char* argv[])
+inline void ProcessCommandArguments(int argc, char* argv[])
 {
-	window::HideConsole(); // by default hide the console
-
 	for (int i = 0; i < argc; i++) { // check for command line arguments
 		if (strcmp(argv[i], "--debug") == 0 || strcmp(argv[i], "-d") == 0) {
 			window::ShowConsole();
 			break;
 		}
 	}
+}
+
+int main(int argc, char* argv[])
+{
+	window::HideConsole(); // by default hide the console
+
+	ProcessCommandArguments(argc, argv);
 
 	SDL_Event event; // contains window events such as resizing, moving, key press, mouse movments, etc.
 	auto AppManager = application::ApplicationManager(&event);
 	AppManager.CreateApplication<game::Game>();
-
-	AppManager.AddKeyboardInputEvent(SDLK_0, "ZERO");
 
 	while (!AppManager.is_closed) {
 		SDL_PollEvent(&event); // gets whatever the current event is, also updates the active windows--meaning they can be moved and resized, etc.
